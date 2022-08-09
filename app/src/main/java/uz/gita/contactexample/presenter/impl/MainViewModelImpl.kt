72098624c131d36ffe8a75bd5e1dac0b577615a8
Ruzimukhammad Kholkhujayev <xolxujayevv@gmail.com>
2022-08-09 19:13:53 +0500

    -repositry implements

---
+++ app/src/main/java/uz/gita/contactexample/presenter/impl/MainViewModelImpl.kt
@@ -0,0 +1,33 @@
+package uz.gita.contactexample.presenter.impl
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import uz.gita.contactexample.data.model.ContactData
+import uz.gita.contactexample.presenter.MainViewModel
+import uz.gita.contactexample.repository.Repository
+
+class MainViewModelImpl : MainViewModel {
+
+    private val repository = Repository
+
+    override val contactListLiveData = MutableLiveData<List<ContactData>>()
+    override val openAddContactLiveData = MutableLiveData<Unit>()
+    override val openInfoLiveData = MutableLiveData<Unit>()
+    override val openEditLiveData = MutableLiveData<ContactData>()
+
+    override fun openAddContactScreen() {
+        
+    }
+
+    override fun openInfoScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun openEditContactScreen(contactData: ContactData) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteContact(contactData: ContactData) {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/uz/gita/contactexample/repository/Repository.kt
+++ app/src/main/java/uz/gita/contactexample/repository/Repository.kt
@@ -1,5 +1,27 @@
 package uz.gita.contactexample.repository
 
+import androidx.lifecycle.LiveData
+import uz.gita.contactexample.data.model.ContactData
+import uz.gita.contactexample.data.room.AppDatabase
+
 object Repository {
+
+    private val contactDao = AppDatabase.getDatabse().getContactDao()
+
+    fun insert(contactData: ContactData) {
+        contactDao.insert(contactData)
+    }
+
+    fun update(contactData: ContactData){
+        contactDao.update(contactData)
+    }
+
+    fun delete(contactData: ContactData){
+        contactDao.delete(contactData)
+    }
+
+    fun getAllContacts(): LiveData<List<ContactData>>{
+        return contactDao.getAllContacts()
+    }
 
 }
